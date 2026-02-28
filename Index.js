13679193adface1c5489d145022c84d8c3e1c716
Cheesypockets <ken020789@gmail.com>
2026-02-28 12:36:35 -0500

    Update Index.js

--- Index.js
+++ Index.js
@@ -14,6 +14,35 @@ botClient.once(Events.ClientReady, (readyClient) => {
     console.log(`Ready! Logged in as ${readyClient.user.tag}`)
 });
 
-console.log(`\n ${token}`)
-
 botClient.login(token);
+
+//stores a collection of bot commands (writing this downt o rememebr)
+botClient.commands = new Collection();
+
+const foldersPath = path.join(__dirname, `commands`);
+
+for (const folder of commandFolders){
+    const commandPath = path.join(foldersPath, folder);
+
+    //each "command path" will be a folder, then the file associated would will be attatched.
+    //so utilities will be the command paths then "ping" would be the command file ending in
+    //".js". consequently becomes => /utilities/ping.js
+    const commandFiles = fs.readdirSync(commandPath).filter((file) => file.endsWith(`.js`));
+    for (const file of commandFiles){
+        const filePath = path.join(commandPath, file)
+
+        //the final built filepath will become the source of the command
+        const command = require(filePath)
+
+        if (('data' in command) && (`execute` in command)){
+            botClient.commands.set(command.data.name, command);
+        }
+        else{
+            console.log("file missing/data in commmand file missing")
+        }
+    }
+}
+
+botClient.on(Events.InteractionCreate, (interaction) => {
+    console.log(interaction)
+})
