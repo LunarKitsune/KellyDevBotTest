65c94f88902ec064791e0f50728d6b442ea79c04
Cheesypockets <ken020789@gmail.com>
2026-03-01 23:56:38 -0500

    small changes

---
+++ Logging/Logger.js
@@ -0,0 +1,12 @@
+//will eventually do a file logger here for easier reading when something happens.
+
+//What I want here:
+/*
+    -Errors
+    -Logging for warnings
+    -Any api changes that would have to be made
+    -for this bot.
+
+    -User input and warnings should be a seperate file
+    from errors. This would make the log readings easier. 
+*/
